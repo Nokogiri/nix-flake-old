8160fe45370bfeb86c3f91701cacfa1f1651337a
nokogiri <nokogiri@gefjon.org>
2023-07-11 22:43:44 +0200

    cleaning...

--- nixos/mowteng/local/udev.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }: {
-  services.udev.extraRules = ''
-    ACTION=="add", ATTR{idVendor}=="0c45", ATTR{idProduct}=="6739", RUN="${pkgs.bash}/bin/sh -c 'echo 1 >/sys/\$devpath/remove'"
-  '';
-}
