559995a5c9f590062107278d55591bd63da0432f
nokogiri <nokogiri@gefjon.org>
2023-07-21 17:23:44 +0200

    nix flake update

--- nixos/homeassistant/local/systemd-network.nix
+++ nixos/homeassistant/local/systemd-network.nix
@@ -1,19 +1,19 @@
 {
   imports = [ ../../common/optional/systemd-network.nix ];
   networking.useDHCP = false;
-  systemd.network.netdevs."qbridge" = {
-    netdevConfig = {
-      MACAddress = "68:5b:35:95:d3:8b";
-      Name = "br0";
-      Kind = "bridge";
-    };
-  };
-  systemd.network.networks."10_bind" = {
+  #systemd.network.netdevs."qbridge" = {
+  #  netdevConfig = {
+  #    #MACAddress = "68:5b:35:95:d3:8b";
+  #    Name = "br0";
+  #    Kind = "bridge";
+  #  };
+  #};
+  #systemd.network.networks."10_bind" = {
+  #  matchConfig.Name = [ "en*" ];
+  #  bridge = [ "br0" ];
+  #};
+  systemd.network.networks."wired" = {
     matchConfig.Name = [ "en*" ];
-    bridge = [ "br0" ];
-  };
-  systemd.network.networks."qbridge" = {
-    matchConfig.Name = [ "br0" ];
     DHCP = "yes";
     dhcpV4Config = { UseDomains = true; };
     networkConfig = {
