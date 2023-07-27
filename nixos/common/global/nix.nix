09b5c181df2326bf15bec56f8c05cffed06946ae
nokogiri <nokogiri@gefjon.org>
2023-07-27 17:44:29 +0200

    auto-optimize nix-store

--- nixos/common/global/nix.nix
+++ nixos/common/global/nix.nix
@@ -7,17 +7,15 @@
         "https://nookogiri.cachix.org"
         "https://cache.fishoeder.net"
         "https://hyprland.cachix.org"
-        "https://anyrun.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nookogiri.cachix.org-1:tKY3HBn6Xhuqys5EcQRl1u+sKualOXFzCs9Q6qEXR+o="
         "cache.fishoeder.net:8LJqtsUc2Sdbvucf7HCakDEetd1b2rGLFmVaDF1POdc="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
       trusted-users = [ "root" "@wheel" ];
-      auto-optimise-store = lib.mkDefault false;
+      auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
     };
