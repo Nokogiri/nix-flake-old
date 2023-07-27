d1b2d6c94dffc433d11544bfe442bbd7c3cbf02d
nokogiri <nokogiri@gefjon.org>
2023-07-27 13:11:47 +0200

    more dracula!

--- home-manager/features/desktop/common/firefox/default.nix
+++ home-manager/features/desktop/common/firefox/default.nix
@@ -74,10 +74,22 @@
         "browser.shell.checkDefaultBrowser" = false;
         "browser.shell.defaultBrowserCheckCount" = 1;
         "browser.disableResetPrompt" = true;
-        "browser.uiCustomization.state" = ''
-          {"placements":{"widget-overflow-fixed-list":["ublock0_raymondhill_net-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":17,"newElementCount":3}'';
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "layout.css.color-mix.enabled" = true;
+        "browser.tabs.tabMinWidth" = 66;
+        "browser.tabs.tabClipWidth" = 86;
+        "browser.tabs.tabmanager.enabled" = false;
+        #"browser.uiCustomization.state" = ''
+        #  {"placements":{"widget-overflow-fixed-list":["ublock0_raymondhill_net-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":17,"newElementCount":3}'';
       };
 
+      userChrome = ''
+       @import url("/home/nokogiri/.mozilla/dracula/userChrome.css");
+      '';
+      userContent = ''
+        @import url("/home/nokogiri/.mozilla/dracula/userContent.css");
+      '';
     };
   };
 }
