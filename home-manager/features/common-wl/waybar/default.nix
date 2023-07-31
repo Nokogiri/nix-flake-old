c5416fc06832e614c0175e9e4276d1e0f8c784bb
nokogiri <nokogiri@gefjon.org>
2023-07-31 11:12:18 +0200

    integrating waybar

--- home-manager/features/common-wl/waybar/default.nix
+++ home-manager/features/common-wl/waybar/default.nix
@@ -5,6 +5,7 @@
   programs.waybar = {
     enable = true;
     package = inputs.hyprland.packages.x86_64-linux.waybar-hyprland;
+    style = ./style.css;
     settings = {
       mainBar = {
         
