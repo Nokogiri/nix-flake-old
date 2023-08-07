53dbb43075c2e1b4f0431fe18fb5d16d316dbcce
nokogiri <nokogiri@gefjon.org>
2023-08-07 09:27:20 +0200

    migrating hyprland config to nix style

---
+++ home-manager/features/hyprland/mowteng.nix
@@ -0,0 +1,17 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1,preferred,auto,1.2";
+      ",preferred,auto,1";
+    ];
+    input = {
+      touchdevice = {
+        output = "eDP-1";
+      };
+    };
+    "device:wacom-hid-49c8-pen" = {
+      output = "eDP-1";
+      enabled = true;
+    };
+  };
+}
