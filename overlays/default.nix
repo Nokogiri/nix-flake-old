2a5a0ba704109869a829e65f4888b2fe39a7b316
nokogiri <nokogiri@gefjon.org>
2023-09-01 00:05:05 +0200

    cleanups

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,21 +8,7 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
-    #pfetch = prev.pfetch.overrideAttrs (oldAttrs: {
-    #  version = "unstable-2021-12-10";
-    #  src = final.fetchFromGitHub {
-    #    owner = "dylanaraps";
-    #    repo = "pfetch";
-    #    rev = "a906ff89680c78cec9785f3ff49ca8b272a0f96b";
-    #    sha256 = "sha256-9n5w93PnSxF53V12iRqLyj0hCrJ3jRibkw8VK3tFDvo=";
-    #  };
-      # Add term option, rename de to desktop, add scheme option
-    #  patches = (oldAttrs.patches or [ ]) ++ [ ./pfetch.patch ];
-    #});
-    myGTK = prev.catppuccin-gtk.override {
+     myGTK = prev.catppuccin-gtk.override {
       accents = [ "sapphire" ];
       size = "standard";
       tweaks = [ "rimless" ];
@@ -36,7 +22,7 @@
       accent = "sapphire";
       flavor = "frappe";      
     };    
-    };
+  };
 
   #master-pkgs = self: super: {
 
