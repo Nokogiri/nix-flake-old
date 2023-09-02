0e08ef3fa0c5889e28a3e7f47e8f7fdef8a7d2cd
nokogiri <nokogiri@gefjon.org>
2023-09-02 21:48:28 +0200

    fuzzel dracula

--- home-manager/features/common-wl/fuzzel.nix
+++ home-manager/features/common-wl/fuzzel.nix
@@ -35,17 +35,16 @@
 
 
     [colors]
-    background=303446aa
-    text=C6D0F5ff
-    match=a6d189ff
-    selection=303446ff
-    selection-text=81c8beff
-    selection-match=ef9f76ff
-    border=303446ff
-    # border=002b36ff
+    background=282a36dd
+    text=f8f8f2ff
+    match=8be9fdff
+    selection-match=8be9fdff
+    selection=44475add
+    selection-text=f8f8f2ff
+    border=bd93f9ff
 
     [border]
-    width=1
+    width=0
     radius=3
 
     [dmenu]
