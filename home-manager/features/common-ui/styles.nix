02c1b5c23d252e735afbd524f2c9458b3a6012a9
nokogiri <nokogiri@gefjon.org>
2023-09-05 16:31:38 +0200

    misc

--- home-manager/features/common-ui/styles.nix
+++ home-manager/features/common-ui/styles.nix
@@ -27,7 +27,7 @@ in {
     enable = true;
     cursorTheme = { name = "Nordzy-cursors-white"; };
     font = {
-      name = "Noto Sans";
+      name = "SauceCodePro Nerd Font";
       size = 10;
     };
     iconTheme = { name = "Dracula"; package = pkgs.dracula-icon-theme;  };
