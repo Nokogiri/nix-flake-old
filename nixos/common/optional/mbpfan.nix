35de781f192ed693a6f917c1ceb0fec2437e4d0d
nokogiri <nokogiri@gefjon.org>
2023-09-07 06:37:43 +0200

    nix flake update

--- nixos/common/optional/mbpfan.nix
+++ nixos/common/optional/mbpfan.nix
@@ -2,11 +2,11 @@
   services.mbpfan = {
     enable = true;
     settings.general = {
-      low_temp = 55;
-      high_temp = 70;
+      low_temp = 45;
+      high_temp = 60;
       max_temp = 85;
       polling_interval = 2;
-      min_fan1_speed = 3000;
+      min_fan1_speed = 2000;
     };
   };
 
