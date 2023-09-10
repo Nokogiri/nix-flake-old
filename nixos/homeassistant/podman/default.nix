317307e685af638a53044ec7cf0115a33c9a5be9
nokogiri <nokogiri@gefjon.org>
2023-09-10 20:57:37 +0200

    kitchenowl

--- nixos/homeassistant/podman/default.nix
+++ nixos/homeassistant/podman/default.nix
@@ -4,6 +4,7 @@
     #./calibre-web.nix
     ./esphome.nix
     ./homeassistant.nix
+    ./kitchenowl.nix
     ./pihole.nix
   ];
   virtualisation.oci-containers.backend = "podman";
---
+++ nixos/homeassistant/podman/kitchenowl.nix
@@ -0,0 +1,33 @@
+{
+  virtualisation.oci-containers.containers = {
+    kowl-front = {
+      image = "tombursch/kitchenowl-web:latest";
+      autoStart = true;
+      #restart = "unless-stopped";
+      volumes = [
+        "/var/lib/pods/calibre-web/calibre-data:/config"
+        "/var/lib/pods/calibre-web/books:/books"
+      ];
+      environment = {
+        TZ = "Europe/Berlin";
+        DOCKER_MODS = "linuxserver/mods:universal-calibre";
+        PUID = "1000";
+        PGID = "1000";
+      };
+      ports = [ "10.200.200.1:8099:80/tcp" ];
+      dependsOn = ["kowl-back"];
+    };
+    kowl-back = {
+      image = "tombursch/kitchenowl:latest";
+      autoStart = true;
+      #restart = "unless-stopped";
+      environment = {
+        JWT_SECRET_KEY = "PLEASE_CHANGE_ME";
+      };
+      volumes = [
+        "/var/lib/pods/kitchenowl:/data"
+      ];
+      
+    };
+  };
+}
