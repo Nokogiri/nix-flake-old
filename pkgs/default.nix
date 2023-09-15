b632c174e96d53ed4a6f6b20460ec8626010f5ee
nokogiri <nokogiri@gefjon.org>
2023-09-15 07:10:23 +0200

    patool

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,6 +10,7 @@
   pass-fuzzel = pkgs.callPackage ./pass-fuzzel { };
   pass-wofi = pkgs.callPackage ./pass-wofi { };
   openmw = pkgs.libsForQt5.callPackage ./openmw { };
+  patool = pkgs.python310Packages.callPackage ./patool { };
   portmod = pkgs.callPackage ./portmod { };
   ryzen-ppd = pkgs.callPackage ./ryzen-ppd { };
 }
