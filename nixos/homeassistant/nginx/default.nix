79f57e06c5607522e48497c5d6f7de0bb6f0f2d0
nokogiri <nokogiri@gefjon.org>
2023-09-17 21:44:01 +0200

    nginx cleanups

--- nixos/homeassistant/nginx/default.nix
+++ nixos/homeassistant/nginx/default.nix
@@ -54,69 +54,11 @@
     user = "nokogiri";
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
-    #recommendedProxySettings = true;
-    #recommendedTlsSettings = true;
-
-    #virtualHosts."files.fishoeder.net" = {
-    #  useACMEHost = "fishoeder.net";
-    #  forceSSL = true;
-    #  root = "/srv/www/files";
-    #};
 
     virtualHosts."git.fishoeder.net" = {
       useACMEHost = "fishoeder.net";
       forceSSL = true;
       root = "/srv/www/git";
     };
-
-    #virtualHosts = {
-    #  "vault.fishoeder.net" = {
-    #    forceSSL = true;
-    #    useACMEHost = "fishoeder.net";
-    #    locations."/" = {
-    #      proxyPass =
-    #        "http://localhost:8812"; # changed the default rocket port due to some conflict
-    #      proxyWebsockets = true;
-    #    };
-    #    locations."/notifications/hub" = {
-    #      proxyPass = "http://localhost:3012";
-    #      proxyWebsockets = true;
-    #    };
-    #    locations."/notifications/hub/negotiate" = {
-    #      proxyPass = "http://localhost:8812";
-    ##      proxyWebsockets = true;
-    #    };
-    #  };
-    #};
-
-    #virtualHosts."dav.fishoeder.net" = {
-    #  forceSSL = true;
-    #  useACMEHost = "fishoeder.net";
-    #  locations."/" = {
-    #    proxyPass = "http://127.0.0.1:8765";
-    #    extraConfig = "proxy_buffering off;" + "proxy_read_timeout    120s;"
-    #      + "proxy_connect_timeout 90s;" + "proxy_send_timeout    90s;"
-    #      + "proxy_redirect        off;" + "proxy_set_header      Host $host;"
-    #      + "proxy_set_header      X-Forwarded-For $proxy_add_x_forwarded_for;"
-    #      + "proxy_set_header      X-Forwarded-Proto $scheme;";
-    #  };
-    #};
-    ##virtualHosts."media.fishoeder.net" = {
-    #  useACMEHost = "fishoeder.net";
-    #  forceSSL = true;
-    #  locations."/" = {
-    #    proxyPass = "http://10.200.200.1:8096";
-    #    extraConfig = "proxy_redirect off;"
-    #      + "proxy_set_header Range $http_range;"
-    #      + "proxy_set_header If-Range $http_if_range;"
-    #      + "proxy_set_header X-Real-IP $remote_addr;"
-    #      + "proxy_set_header Host $host;"
-    #      + "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
-    #      + "proxy_set_header X-Forwarded-Protocol $scheme;"
-    #      + "proxy_http_version 1.1;"
-    #      + "proxy_set_header Upgrade $http_upgrade;"
-    #      + ''proxy_set_header Connection "upgrade";'';
-    #  };
-    #};
   };
 }
