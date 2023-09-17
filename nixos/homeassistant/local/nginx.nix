2cac3d63c8613744649891d06aa07e9d0872f660
nokogiri <nokogiri@gefjon.org>
2023-09-17 20:59:03 +0200

    nginx

--- nixos/homeassistant/local/nginx.nix
+++ nixos/homeassistant/local/nginx.nix
@@ -1,5 +1,5 @@
 { config, ... }: {
-  imports = [ (./sites/foodwiki.nix) (./sites/cache.nix) (./sites/haos.fishoeder.net) ];
+  imports = [ (./sites/foodwiki.nix) (./sites/cache.nix) (./sites/haos.fishoeder.net.nix) ];
 
   sops.secrets.ovh_dns = {
     sopsFile = ../../common/secrets.yaml;
