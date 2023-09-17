1618a3325fd9277e2f81b5359e7c93bb009fcccc
nokogiri <nokogiri@gefjon.org>
2023-09-17 22:07:15 +0200

    cleanups

---
+++ nixos/homeassistant/services/default.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ./cgit.nix
+    ./dave.nix
+    #./gitea.nix
+    ./nfs-server.nix
+    ./nix-serve.nix
+    ./samba.nix
+    ./snapserver.nix
+    ./postgresql.nix
+    ./jellyfin.nix
+    ./transmission.nix
+  ];
+}
