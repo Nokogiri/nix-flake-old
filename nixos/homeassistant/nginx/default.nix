530a9d4daa326be89f1ca012617452aa47237ab5
nokogiri <nokogiri@gefjon.org>
2023-09-17 21:47:19 +0200

    nginx cleanups

--- nixos/homeassistant/nginx/default.nix
+++ nixos/homeassistant/nginx/default.nix
@@ -55,11 +55,5 @@
     user = "nokogiri";
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
-
-    #virtualHosts."git.fishoeder.net" = {
-    #  useACMEHost = "fishoeder.net";
-    #  forceSSL = true;
-    #  root = "/srv/www/git";
-    #};
   };
 }
