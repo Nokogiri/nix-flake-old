178c4feeaf1618cd92013eb4be551dc96434a033
nokogiri <nokogiri@gefjon.org>
2023-09-17 20:58:17 +0200

    nginx

---
+++ nixos/homeassistant/local/sites/haos.fishoeder.net.nix
@@ -0,0 +1,28 @@
+{
+    services.nginx = {
+    enable = true;
+    user = "nokogiri";
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    #recommendedProxySettings = true;
+    #recommendedTlsSettings = true;
+
+    virtualHosts."haos.fishoeder.net" = {
+      useACMEHost = "fishoeder.net";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://192.168.178.57:8123";
+        extraConfig = "proxy_redirect off;"
+          + "proxy_set_header Range $http_range;"
+          + "proxy_set_header If-Range $http_if_range;"
+          + "proxy_set_header X-Real-IP $remote_addr;"
+          + "proxy_set_header Host $host;"
+          + "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
+          + "proxy_set_header X-Forwarded-Protocol $scheme;"
+          + "proxy_http_version 1.1;"
+          + "proxy_set_header Upgrade $http_upgrade;"
+          + ''proxy_set_header Connection "upgrade";'';
+      };
+    };
+  }; 
+}
