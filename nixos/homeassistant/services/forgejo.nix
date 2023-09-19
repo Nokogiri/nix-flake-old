dce21e880ece6952c6e7859445ed7ea80d5714e0
nokogiri <nokogiri@gefjon.org>
2023-09-19 23:09:45 +0200

    forgejo

--- nixos/homeassistant/services/forgejo.nix
+++ nixos/homeassistant/services/forgejo.nix
@@ -1,7 +1,15 @@
 { pkgs, config, ... }: {
-  sops.secrets.forgejo_mail = { sopsFile = ../../common/secrets.yaml; owner = config.services.forgejo.user; group = config.services.forgejo.group; };
-  sops.secrets.forgejo_pg = { sopsFile = ../../common/secrets.yaml; owner = config.services.forgejo.user; group = config.services.forgejo.group; };
-  
+  sops.secrets.forgejo_mail = {
+    sopsFile = ../../common/secrets.yaml;
+    owner = config.services.forgejo.user;
+    group = config.services.forgejo.group;
+  };
+  sops.secrets.forgejo_pg = {
+    sopsFile = ../../common/secrets.yaml;
+    owner = config.services.forgejo.user;
+    group = config.services.forgejo.group;
+  };
+
   services.forgejo = {
     enable = true;
     package = pkgs.forgejo;
@@ -12,19 +20,17 @@
     };
     mailerPasswordFile = config.sops.secrets.forgejo_mail.path;
     settings = {
-      session = {
-        COOKIE_SECURE = true;
-      };
-      server = {
-	DISABLE_REGISTRATION = true;
-      };
+      session = { COOKIE_SECURE = true; };
+      server = { DISABLE_REGISTRATION = true; };
       server = {
         DOMAIN = "git.fishoeder.net";
         PROTOCOL = "http+unix";
-	ROOT_URL = "https://git.fishoeder.net";
+        ROOT_URL = "https://git.fishoeder.net";
+        STATIC_URL_PREFIX = "/_/static";
       };
       repository = {
         ENABLE_PUSH_CREATE_USER = true;
+        DEFAULT_PUSH_CREATE_PRIVATE = false;
       };
       mailer = {
         ENABLED = true;
@@ -34,9 +40,7 @@
         USER = "nokogiri@gefjon.org";
         PROTOCOL = "smtps";
       };
-      other = {
-        SHOW_FOOTER_VERSION = false;
-      };
+      other = { SHOW_FOOTER_VERSION = false; };
     };
   };
   services.nginx = {
@@ -47,8 +51,12 @@
       "git.fishoeder.net" = {
         forceSSL = true;
         useACMEHost = "fishoeder.net";
+        locations."/_/static/assets/" = {
+          alias = "${config.services.forgejo.package.data}";
+        };
         locations."/" = {
-          proxyPass = "http://unix:/${config.services.forgejo.settings.server.HTTP_ADDR}";
+          proxyPass =
+            "http://unix:/${config.services.forgejo.settings.server.HTTP_ADDR}";
           extraConfig = "proxy_set_header Host $host;"
             + "proxy_set_header X-Real-IP $remote_addr;"
             + "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
