38df54785f5e04f7c88bfbea088ff7c488cd38a4
nokogiri <nokogiri@gefjon.org>
2023-09-20 07:07:34 +0200

    localhost → 127.0.0.1

--- nixos/homeassistant/services/vaultwarden.nix
+++ nixos/homeassistant/services/vaultwarden.nix
@@ -28,15 +28,15 @@
         useACMEHost = "fishoeder.net";
         locations."/" = {
           proxyPass =
-            "http://localhost:8812"; # changed the default rocket port due to some conflict
+            "http://127.0.0.1:8812"; # changed the default rocket port due to some conflict
           proxyWebsockets = true;
         };
         locations."/notifications/hub" = {
-          proxyPass = "http://localhost:3012";
+          proxyPass = "http://127.0.0.1:3012";
           proxyWebsockets = true;
         };
         locations."/notifications/hub/negotiate" = {
-          proxyPass = "http://localhost:8812";
+          proxyPass = "http://127.0.0.1:8812";
           proxyWebsockets = true;
         };
       };
