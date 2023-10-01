029ed662e8beabe40396695d7f7eec1b8fe7637a
nokogiri <nokogiri@gefjon.org>
2023-10-01 22:56:03 +0200

    nix flake update

--- nixos/common/global/nix.nix
+++ nixos/common/global/nix.nix
@@ -4,24 +4,20 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
-        "https://nookogiri.cachix.org"
         "https://cache.fishoeder.net"
-        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nookogiri.cachix.org-1:tKY3HBn6Xhuqys5EcQRl1u+sKualOXFzCs9Q6qEXR+o="
         "cache.fishoeder.net:8LJqtsUc2Sdbvucf7HCakDEetd1b2rGLFmVaDF1POdc="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
-      trusted-users = [ "root" "@wheel" ];
+      trusted-users = [ "root" "@wheel" "nokogiri" ];
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
     };
     package = pkgs.nixUnstable;
     gc = {
-      automatic = lib.mkDefault true;
+      automatic = lib.mkDefault false;
       dates = "monthly";
     };
 
