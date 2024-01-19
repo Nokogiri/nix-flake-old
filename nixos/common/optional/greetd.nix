7e738d1e888235ce26ed11013d22dd56eda99dbb
nokogiri <nokogiri@gefjon.org>
2024-01-19 20:21:09 +0100

    slighty progressing

--- nixos/common/optional/greetd.nix
+++ nixos/common/optional/greetd.nix
@@ -1,12 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let user = "nokogiri";
 in {
+  environment.systemPackages = [ pkgs.hyprland ];
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd '$SHELL -l'";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r --time --cmd '$SHELL -l'";
+        #command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
         user = "greeter";
+      };
+    };
+  };
+  programs.regreet = {
+    enable = true;
+    settings = {
+      background = {
+        path = "${pkgs.Imaginarium}/wallpaper/FireWatch.png";
       };
     };
   };
