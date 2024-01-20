016269a62ae681a4539aad9c35476ff2232fa1e3
nokogiri <nokogiri@gefjon.org>
2024-01-20 15:47:08 +0100

    slighty progressing

--- nixos/common/optional/quietboot.nix
+++ nixos/common/optional/quietboot.nix
@@ -15,7 +15,7 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
-      "vt.global_cursor_default=0"
+      #"vt.global_cursor_default=0"
     ];
     consoleLogLevel = 0;
     initrd.verbose = false;
