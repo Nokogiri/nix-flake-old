80921bcd45d436e728ba6c67aa6f0324c8a3088a
nokogiri <nokogiri@gefjon.org>
2024-02-11 20:59:46 +0100

    cleanup steam

--- home-manager/features/games/steam.nix
+++ home-manager/features/games/steam.nix
@@ -4,29 +4,15 @@
       steam = steam.override {
         extraPkgs = pkgs:
           with pkgs; [
-            attr
             gst_all_1.gst-vaapi
             gst_all_1.gst-plugins-good
             gst_all_1.gst-plugins-bad
             gst_all_1.gst-plugins-ugly
             gst_all_1.gst-libav
-            gperftools
-            libgdiplus
             gamescope
             gamemode
             fribidi
             mangohud
-            mono
-            nss
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-            xorg.libXdmcp
-            xorg.libXau
-            libpng
-            gtk3
-            gtk3-x11
             libgdiplus
             zlib
             libpulseaudio
@@ -37,12 +23,7 @@
             keyutils
             xdg-user-dirs
             inputs.extest.packages.x86_64-linux.default
-            #STL
-            unixtools.xxd
             wget
-            xdotool
-            xorg.xprop
-            xorg.xwininfo
             yad
           ];
         extraProfile = ''
