7683fa80cbff2deb1854f1aad5fbe9363be755f9
nokogiri <nokogiri@gefjon.org>
2024-03-09 19:59:54 +0100

    cachix hyprland

--- nixos/common/global/default.nix
+++ nixos/common/global/default.nix
@@ -33,12 +33,14 @@
   nix = {
     settings = {
       substituters = [
-        #"https://nix-community.cachix.org"
+        "https://nix-community.cachix.org"
         #"https://ezkea.cachix.org"
         #"https://anyrun.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
         "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
