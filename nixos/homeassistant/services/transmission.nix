12c3607dfdb31f12a75b9da296235f53b926445b
nokogiri <nokogiri@gefjon.org>
2024-05-05 20:01:28 +0200

    nx flake update && fixes

--- nixos/homeassistant/services/transmission.nix
+++ nixos/homeassistant/services/transmission.nix
@@ -16,6 +16,8 @@
         incomplete-dir = "/media/Vault3.1/downloads/.incomplete";
         download-dir = "/media/Vault3.1/downloads";
         rpc-bind-address = "10.200.200.1";
+        speed-limit-up = "0";
+        speed-limit-up-enabled = true;
       };
     };
   };
