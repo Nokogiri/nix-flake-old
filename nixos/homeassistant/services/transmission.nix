98e0257c28b7ac2d11e6e0f0370a5c9740d48c70
nokogiri <nokogiri@gefjon.org>
2024-05-05 21:39:35 +0200

    glib

--- nixos/homeassistant/services/transmission.nix
+++ nixos/homeassistant/services/transmission.nix
@@ -16,7 +16,7 @@
         incomplete-dir = "/media/Vault3.1/downloads/.incomplete";
         download-dir = "/media/Vault3.1/downloads";
         rpc-bind-address = "10.200.200.1";
-        speed-limit-up = 0;
+        #speed-limit-up = 0;
         speed-limit-up-enabled = true;
       };
     };
